19d9db30fe922084ccdd6b55417965e300ec8520
Umberto Zerbinati <uzerbinati@me.com>
2020-11-27 22:50:48 +0100

    Changed theme ...

--- Docs/conf.py
+++ Docs/conf.py
@@ -35,8 +35,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
+html_theme_path = ["_themes", ]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
