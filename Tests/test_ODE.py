84d9d03f7e7712b9843884a04aa4f24b10da4d21
Umberto Zerbinati <uzerbinati@me.com>
2020-12-21 17:48:50 +0100

    Added testing for basic ODE

---
+++ Tests/test_ODE.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../Py/Build');
+tol = 1e-4;
+from math import exp
+from suite import *
+def test_ODE_basic():
+   print("Testing for u'=u, u(0)=1"); 
+   DiffEq = LinearODE(1);
+   DiffEq.setDomain(0,2);
+   DiffEq.setCoeff(0,lambda t : 0)
+   DiffEq.setCoeff(1,lambda t : -1)
+   DiffEq.setIC(1.0)
+   [H,u] = DiffEq.ScalarEuler(1e-6);
+   assert (abs(u[-1]-exp(2))< tol);
+
