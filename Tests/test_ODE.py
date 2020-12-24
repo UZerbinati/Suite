2a873010eaf24f18509000fb21845eac6027e3bc
Umberto Zerbinati <uzerbinati@me.com>
2020-12-24 16:06:27 +0100

    pytest modify ode run time

--- Tests/test_ODE.py
+++ Tests/test_ODE.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../Py/Build');
-tol = 1e-6;
+tol = 1e-4;
 from math import exp
 from suite import *
 import pytest
@@ -11,6 +11,6 @@ def test_ODE_basic():
    DiffEq.setCoeff(0,lambda t : 0)
    DiffEq.setCoeff(1,lambda t : -1)
    DiffEq.setIC(1.0)
-   [H,u] = DiffEq.ScalarEuler(1e-8);
+   [H,u] = DiffEq.ScalarEuler(1e-6);
    assert (abs(u[-1]-exp(2))< tol);
 
